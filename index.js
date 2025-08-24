e0c5eea15cf69a49cff44dc77ab41d5e9448182d
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-08-24 12:01:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import login from "fca-priyansh";
 import fs from "fs";
 import express from "express";
 
-const OWNER_UIDS = ["61567677313211", "61567677313211"];
+const OWNER_UIDS = ["61579589454986", "61579589454986"];
 let rkbInterval = null;
 let stopRequested = false;
 const lockedGroupNames = {};
